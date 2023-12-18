2c03e57ed10a99d99e2530835930b03ddb7e255d
LWL220393060 <145098154+220393060-LEEHOLUN@users.noreply.github.com>
2023-12-18 19:32:45 +0800

    commit

---
+++ client/src/app/product/product.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h1>Product List</h1>
+            <ProductList />
+        </div>
+    );
+};
+
+export default ProductPage;
