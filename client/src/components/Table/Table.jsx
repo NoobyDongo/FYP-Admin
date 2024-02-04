50f70247f367c322caff8f202b30b9b6d97dbd7e
LWL220393060 <145098154+220393060-LEEHOLUN@users.noreply.github.com>
2024-02-04 18:58:59 +0800

    Revert "Merge branch 'main' of https://github.com/NoobyDongo/FYP-Client"
    This reverts commit 88c480783fbc33a2d3b519fa867ed809cd670c6c, reversing
    changes made to f8e3a0bd8b5c8e8e4f13577285dc79fa783895ed.

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -97,8 +97,6 @@ export const Table = (props) => {
 
     const [isFullScreen, setIsFullScreen] = React.useState(false)
 
-    const [isFullScreen, setIsFullScreen] = React.useState(false)
-
     const table = useMaterialReactTable({
 <<<<<<< HEAD
         columns: columns,
